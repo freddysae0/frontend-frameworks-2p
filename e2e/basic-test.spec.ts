3ecf54f09bc74c9be26d8d3804d0af1929ca2e96
Freddy Saez <freddysae00@gmail.com>
2025-03-28 15:40:46 +0100

    Making e2e test just one function

--- e2e/basic-test.spec.ts
+++ e2e/basic-test.spec.ts
@@ -1,73 +1,45 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Shrek Adjective Changer - - - REACT TEST", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("http://127.0.0.1:4950");
+const VUE_PORT = 4951;
+const REACT_PORT = 4950;
+
+const testingFn = (app: "Vue" | "React") => {
+  const port = app === "Vue" ? VUE_PORT : REACT_PORT;
+  test.describe(`Shrek Adjective Changer - - - ${app} TEST`, () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(`http://127.0.0.1:${port}`);
+    });
+
+    test("Debe mostrar un adjetivo inicial", async ({ page }) => {
+      const adjectiveSpan = await page.locator(
+        '[data-testid="adjective-span"]'
+      );
+      await expect(adjectiveSpan).toBeVisible();
+    });
+
+    test("Adjetive must change on click at the button", async ({ page }) => {
+      const adjectiveSpan = page.locator('[data-testid="adjective-span"]');
+      const firstAdjective = await adjectiveSpan.textContent();
+
+      await page.click("button");
+
+      await expect(adjectiveSpan).not.toHaveText(firstAdjective!);
+    });
+
+    test("It must return to the first adjetive after pass throw everyone", async ({
+      page,
+    }) => {
+      const adjectives = require("../react/src/app/shrek_adjetives.json");
+      const button = page.locator('[data-testid="shrek-button"]');
+      const adjectiveSpan = page.locator('[data-testid="adjective-span"]');
+
+      for (let i = 0; i < adjectives.length; i++) {
+        await button.click();
+      }
+
+      await expect(adjectiveSpan).toHaveText(adjectives[0]);
+    });
   });
-
-  test("Debe mostrar un adjetivo inicial", async ({ page }) => {
-    const adjectiveSpan = await page.locator('[data-testid="adjective-span"]');
-    await expect(adjectiveSpan).toBeVisible();
-  });
-
-  test("Debe cambiar el adjetivo al hacer clic en el botón", async ({
-    page,
-  }) => {
-    const adjectiveSpan = page.locator('[data-testid="adjective-span"]');
-    const firstAdjective = await adjectiveSpan.textContent();
-
-    await page.click("button");
-
-    await expect(adjectiveSpan).not.toHaveText(firstAdjective!);
-  });
-
-  test("Debe volver al primer adjetivo después de recorrer todos", async ({
-    page,
-  }) => {
-    const adjectives = require("../react/src/app/shrek_adjetives.json");
-    const button = page.locator('[data-testid="shrek-button"]');
-    const adjectiveSpan = page.locator('[data-testid="adjective-span"]');
-
-    for (let i = 0; i < adjectives.length; i++) {
-      await button.click();
-    }
-
-    await expect(adjectiveSpan).toHaveText(adjectives[0]);
-  });
-});
-
-test.describe("Shrek Adjective Changer - - - VUE TEST", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("http://127.0.0.1:4951");
-  });
-
-  test("Debe mostrar un adjetivo inicial", async ({ page }) => {
-    const adjectiveSpan = await page.locator('[data-testid="adjective-span"]');
-    await expect(adjectiveSpan).toBeVisible();
-  });
-
-  test("Debe cambiar el adjetivo al hacer clic en el botón", async ({
-    page,
-  }) => {
-    const adjectiveSpan = page.locator('[data-testid="adjective-span"]');
-    const firstAdjective = await adjectiveSpan.textContent();
-
-    await page.click("button");
-
-    await expect(adjectiveSpan).not.toHaveText(firstAdjective!);
-  });
-
-  test("Debe volver al primer adjetivo después de recorrer todos", async ({
-    page,
-  }) => {
-    const adjectives = require("../react/src/app/shrek_adjetives.json");
-    const button = page.locator('[data-testid="shrek-button"]');
-    const adjectiveSpan = page.locator('[data-testid="adjective-span"]');
-
-    for (let i = 0; i < adjectives.length; i++) {
-      await button.click();
-    }
-
-    await expect(adjectiveSpan).toHaveText(adjectives[0]);
-  });
-});
+};
+testingFn("React");
+testingFn("Vue");
